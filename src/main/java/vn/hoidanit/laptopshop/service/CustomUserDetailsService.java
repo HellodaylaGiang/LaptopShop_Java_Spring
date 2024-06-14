b4dcf471a402843ded8ce7f6d879ec2eb7f0f869
Bún Đậu <124412911+HellodaylaGiang@users.noreply.github.com>
2024-06-14 16:51:11 +0700

    last

--- src/main/java/vn/hoidanit/laptopshop/service/CustomUserDetailsService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/CustomUserDetailsService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-
+    // hi
     private final UserService userService;
 
     public CustomUserDetailsService(UserService userService) {
